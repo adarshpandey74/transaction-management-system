f8528fedf6b0aef3abe5cccb3beda748afe3580e
Adarsh Pandey <adarshpandey74084@gmail.com>
2026-04-07 01:08:19 +0530

    Initial Spring Boot setup with Hello API

---
+++ src/test/java/com/adarsh/transaction_system/TransactionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsh.transaction_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
