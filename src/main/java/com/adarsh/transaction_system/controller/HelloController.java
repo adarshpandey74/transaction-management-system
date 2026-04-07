91b6b96219c2a77e00f16bdbedc93dc661ffb9d1
Adarsh Pandey <adarshpandey74084@gmail.com>
2026-04-08 01:37:28 +0530

    Day 3: Added GET & POST endpoints, connected HTML form to backend, fixed mapping and security issues

--- src/main/java/com/adarsh/transaction_system/controller/HelloController.java
+++ src/main/java/com/adarsh/transaction_system/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.adarsh.transaction_system.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 public class HelloController {
@@ -9,5 +11,25 @@ public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello Banking System 🚀";
+    }
+    @GetMapping("/continue")
+    public String con(){
+        return "It is working";
+    }
+//    @GetMapping("/")
+//    public String home() {
+//        return "Home page working!";
+//    }
+    @GetMapping("/about")
+    public String about() {
+        return "This is my first Spring Boot project";
+    }
+    @GetMapping("/hello-user")
+    public String hello(@RequestParam String name) {
+        return "Hello " + name;
+    }
+    @PostMapping("/hello-user")
+    public String helloPost(@RequestParam String name,@RequestParam int age) {
+        return "Hello " + name + ", age " + age;
     }
 }
